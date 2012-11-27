57dbf66951833fdf2576ba5b2f84d917056e237f
simlegate <simlegate@163.com>
2012-11-28 03:18:48 +0800

    add push function

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -10,10 +10,10 @@ class StoriesController < ApplicationController
   # POST /stories
   # POST /stories.json
   def create
-     PrivatePub.publish_to("/messages/new", message: "success")
+     log = Log.new_message "simlegate create a story of '#{params[:story][:title]}'"
+    #publish_message("messages/new",log)
+     PrivatePub.publish_to("/messages/new", message: log)
      render :json => Story.add_story(params[:story])
-     
-    #render :json => Story.create(status: "22")
   end
 
 
@@ -32,9 +32,19 @@ class StoriesController < ApplicationController
    end
  
    # change status of story
+   # params[:status] stand for next status
    def change_status 
-     result = Story.set_story_status params[:story_id],params[:status]
+     story = Story.get_story_by_story_id(params[:story_id])
+     next_status = get_next_status(params[:status])
+     log = Log.new_message "#{story.title} be to #{params[:status]} by #{current_user.email}" 
+     PrivatePub.publish_to("/messages/new", message: log)
+     result = Story.set_story_status params[:story_id],params[:status],next_status
      result ? (render :json => "success".to_json) : (render :json => "false".to_json)
+   end
+
+   def check
+     PrivatePub.publish_to("/messages/new", message: "success")
+     render :json => "22".to_json
    end
 
 end
