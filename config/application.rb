2bd8a817ccf0f12d87ff57eb1813ea458c51d893
simlegate <simlegate@163.com>
2012-12-02 05:02:49 +0800

    nothing

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module StorycafeSimple
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
-
+    config.channel_prefix = "/channels/"
   end
 end
